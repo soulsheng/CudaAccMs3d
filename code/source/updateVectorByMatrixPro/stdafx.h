77f02107bb9e2e388cac311ede34ede0a54b0950
study <1806597655@qq.com>
2012-10-15 19:29:53 +0800

    补充，数据量低于2k时，时间恒定不变，时间是0.027ms(gts250); 本实验，数据量8k，时间是0.03ms(gts250);

--- code/source/updateVectorByMatrixPro/stdafx.h
+++ code/source/updateVectorByMatrixPro/stdafx.h
@@ -11,10 +11,10 @@
 #include <tchar.h>
 
 
-#define    SCALE_CLASS    8 //1	2	3	4	5	6	7	8	9
-#define    BASE_SIZE     625
-#define    JOINT_SIZE    (1<<((SCALE_CLASS-1)*2))
+#define    SCALE_CLASS    2 //1  2  3	4	5	6	7	8	9 10
+#define    BASE_SIZE     1
+#define    JOINT_SIZE    ( 1 << (SCALE_CLASS*2)  )
 // 数据规模	(M)= JOINT_SIZE * BASE_SIZE * sizeof()	
-// 0.03	0.13	0.5	2	8	32	128	512	2048
+// 0.002   0.008   0.03	0.13	0.5	2	8	32	128	512	2048
 
 // TODO: 在此处引用程序需要的其他头文件
