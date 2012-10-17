f3492972192d855c04a65439ee8328f5dacb7dad
study <1806597655@qq.com>
2012-10-17 11:18:46 +0800

    参数改变，指针数组的管理对象的引用，改为指向指针数组的指针。结果照样正确。

--- code/source/testPointerArray/testPointerArray.cpp
+++ code/source/testPointerArray/testPointerArray.cpp
@@ -7,6 +7,25 @@
 PointerArray	pa;
 PointerArray	paRef;
 
+#define PARAMETER_POINTER	1
+
+#if PARAMETER_POINTER
+void updatePointerArray(float** pa, float** paRef)
+{
+	for (int i=0;i<SIZE_ARRAY;i++)
+	{
+		memcpy( pa[i], paRef[i], sizeof(float)*SIZE_BUFFER );
+	}
+
+	for (int i=0;i<SIZE_ARRAY;i++)
+	{
+		for (int j=0;j<SIZE_BUFFER;j++)
+		{
+			pa[i][j] += 10000.f;
+		}
+	}
+}
+#else
 void updatePointerArray(PointerArray& p, PointerArray& pRef)
 {
 	for (int i=0;i<SIZE_ARRAY;i++)
@@ -22,13 +41,18 @@ void updatePointerArray(PointerArray& p, PointerArray& pRef)
 		}
 	}
 }
+#endif
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	pa.initialize();
 	paRef.initialize();
 
+#if PARAMETER_POINTER
+	updatePointerArray(pa._pointerArray, paRef._pointerArray);
+#else
 	updatePointerArray(pa, paRef);
+#endif
 
 	pa.unInitialize();
 	paRef.unInitialize();
