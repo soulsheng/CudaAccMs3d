901351bbb1eef9edd5672bba5d4569a0802990fc
study <1806597655@qq.com>
2012-10-17 17:25:09 +0800

    对顶点按矩阵索引排序，同一个warp访问同一个矩阵，效率降低，最好访问相邻矩阵。

--- code/source/updateVectorByMatrixPro/Vertex.h
+++ code/source/updateVectorByMatrixPro/Vertex.h
@@ -2,6 +2,9 @@
 
 #include "Joint.h"
 #include "cuda_runtime.h"
+#include <map>
+
+#define		SORT_ARRAY			0// 顶点按矩阵索引排序：0不排序，1排序
 
 //顶点坐标---------------------------------------------------------
 //typedef float4 Vertex; // 坐标：(x,y,z);关节索引：w
@@ -22,6 +25,30 @@ struct Vertexes{
 		cudaMemcpy( pVertexDevice, pVertex, sizeof(Vector4) * nSize, cudaMemcpyHostToDevice );
 
 	}
+	void sort()
+	{
+		int i=0;
+		std::multimap<int, int> indexMatrix;
+		for( i=0;i<nSize;i++){
+			int indexM = int(pVertex[i].w + 0.5);// float to int
+			indexMatrix.insert( std::make_pair(indexM, i) );
+		}
+
+		int *pIndexMatrix = new int[nSize];
+		i=0;
+		for(std::multimap<int, int>::iterator itr = indexMatrix.begin(); itr!=indexMatrix.end(); itr++, i++){
+			pIndexMatrix[i] = itr->second;
+		}
+
+		Vector4* pVertexTemp = new Vector4[nSize];
+		for( i=0;i<nSize;i++){
+			pVertexTemp[i] = pVertex[ pIndexMatrix[i] ];
+		}
+
+		memcpy( pVertex, pVertexTemp, sizeof(float4) * nSize );
+		delete[] pVertexTemp;
+		delete[] pIndexMatrix;
+	}
 
 	// 获取顶点坐标 模拟
 	void initialize(int size, int sizeJoint){
@@ -33,7 +60,9 @@ struct Vertexes{
 			pVertex[i].z = rand() * 1.0f;
 			pVertex[i].w = rand() % sizeJoint  * 1.0f;
 		}
-
+#if SORT_ARRAY
+		sort();
+#endif
 		cudaMalloc( &pVertexDevice, sizeof(Vector4) * nSize ) ;//Vertex[nSize];
 		cudaMemcpy( pVertexDevice, pVertex, sizeof(Vector4) * nSize, cudaMemcpyHostToDevice );
 
