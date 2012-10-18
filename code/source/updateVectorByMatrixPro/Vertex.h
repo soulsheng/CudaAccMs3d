eafa5360237c719dcb4aa212c9cfc177bb43af10
soulsheng <soulsheng@qq.com>
2012-10-18 08:27:13 +0800

    顶点预处理，按照矩阵索引循环交替排序，为矩阵构造合并访问的条件，效率提升10%(gtx670)。

--- code/source/updateVectorByMatrixPro/Vertex.h
+++ code/source/updateVectorByMatrixPro/Vertex.h
@@ -3,8 +3,10 @@
 #include "Joint.h"
 #include "cuda_runtime.h"
 #include <map>
+#include <vector>
 
 #define		SORT_ARRAY			0// 顶点按矩阵索引排序：0不排序，1排序
+#define		SORT_ARRAY_LOOP		0// 顶点按矩阵索引循环交替排序：0不排序，1排序    排序如：0~100,0~100...
 
 //顶点坐标---------------------------------------------------------
 //typedef float4 Vertex; // 坐标：(x,y,z);关节索引：w
@@ -50,6 +52,71 @@ struct Vertexes{
 		delete[] pIndexMatrix;
 	}
 
+	void sortLoop()
+	{
+		typedef std::vector<int> SubIndex;
+		typedef std::vector<int>::iterator SubIndexItr;
+		typedef std::map<int, SubIndex*> IndexMatrix;
+		typedef std::map<int, SubIndex*>::iterator IndexMatrixItr;
+
+		int i=0;
+		IndexMatrix		indexMatrix;
+		IndexMatrixItr	itr;
+		SubIndex		*subIndex;
+
+		// 按组收集索引  形如：0,0,0...0,  1,1,1...1, 2,2,2...2 ,...
+		for (i=0;i<nSizeJoint;i++)
+		{
+			subIndex = new std::vector<int>;
+			indexMatrix.insert(std::make_pair(i,subIndex));
+		}
+
+		for( i=0;i<nSize;i++){
+			int indexM = int(pVertex[i].w + 0.5);// float to int
+			indexMatrix[indexM]->push_back(i);
+		}
+
+		// 构造循环 形如：0,1,2,3,4...16, 0,1,2,3,4...16 ...
+		i=0;
+		int nLengthMax = 0;// 子队列 最大长度
+		for( itr = indexMatrix.begin(); itr!=indexMatrix.end(); itr++){
+			int size = itr->second->size();
+			if(size > nLengthMax) nLengthMax=size ;
+		}
+
+		// 遍历子队列
+		int *pIndexMatrix = new int[nSize];
+		int j=0;
+		for (i=0;i<nLengthMax;i++)
+		{
+			for( itr = indexMatrix.begin(); itr!=indexMatrix.end(); itr++){
+				subIndex = itr->second;
+				if ( i<subIndex->size() )
+				{
+					pIndexMatrix[j++] = (*subIndex)[i];
+				}
+			}
+
+		}
+
+		// 按照循环索引，调整存储
+		Vector4* pVertexTemp = new Vector4[nSize];
+		for( i=0;i<nSize;i++){
+			pVertexTemp[i] = pVertex[ pIndexMatrix[i] ];
+		}
+		memcpy( pVertex, pVertexTemp, sizeof(float4) * nSize );
+		
+		// 释放临时内存空间
+		delete[] pVertexTemp;
+		delete[] pIndexMatrix;
+		
+		for ( itr=indexMatrix.begin();itr!=indexMatrix.end();itr++)
+		{
+			subIndex = itr->second;
+			delete subIndex;
+		}
+	}
+
 	// 获取顶点坐标 模拟
 	void initialize(int size, int sizeJoint){
 		nSize = size;
@@ -63,6 +130,12 @@ struct Vertexes{
 #if SORT_ARRAY
 		sort();
 #endif
+		
+		nSizeJoint = sizeJoint;
+#if SORT_ARRAY_LOOP
+		sortLoop();
+#endif
+
 		cudaMalloc( &pVertexDevice, sizeof(Vector4) * nSize ) ;//Vertex[nSize];
 		cudaMemcpy( pVertexDevice, pVertex, sizeof(Vector4) * nSize, cudaMemcpyHostToDevice );
 
@@ -77,5 +150,6 @@ struct Vertexes{
 
 	Vector4*  pVertex, *pVertexDevice;
 	int   nSize;// 顶点的数目
+	int   nSizeJoint;// 关节的数目
 
 };// 顶点的集合
