3bc2d2e2871576a264deebf2058617c0feaecb77
study <1806597655@qq.com>
2012-10-22 17:19:14 +0800

    动态设置块数，SM个数的2倍。

--- code/source/updateVectorByMatrixPro/updateVectorByMatrixPro.cu
+++ code/source/updateVectorByMatrixPro/updateVectorByMatrixPro.cu
@@ -10,6 +10,10 @@
 #include "updateVectorByMatrixPro.cuh"
 #include "updateVectorByMatrix.h"
 
+#define		SIZE_THREAD	256
+
+int		SIZE_BLOCK=32; // 默认设置，动态设置为SM的两倍，确保每个SM有2个block
+
 float    PROBLEM_SCALE[] ={ 0.25f, 0.5f, 1, 2, 4, 8, 16, 32 }; // 问题规模档次，8档，250K至32M，2倍递增
 int    PROBLEM_SIZE  = MEGA_SIZE * PROBLEM_SCALE[2] ;// 问题规模, 初始设为1M，即一百万
 int iClass=6; // 问题规模最大值，16M/512M显存、32M/1G显存
@@ -24,6 +28,13 @@ void initialize(int problem_size, int joint_size);
 
 // 数据销毁：坐标、矩阵
 void unInitialize();
+
+// 查询每个SM包含GPU核心SP的个数
+int _ConvertSMVer2Cores(int major, int minor);
+
+// 硬件拥有最大的浮点计算能力GFLOPS
+int gpuGetMaxGflopsDeviceId(float& fGFLOPS);
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -45,8 +56,8 @@ int _tmain(int argc, _TCHAR* argv[])
 		{
 			globalMemoryUpdate( &_joints );
 			
-			dim3 nBlocksPerGrid( 64 ); // 块的数目
-			dim3 nThreadsPerBlock( 256 ); // 单块包含线程的数目
+			dim3 nBlocksPerGrid( SIZE_BLOCK ); // 块的数目
+			dim3 nThreadsPerBlock( SIZE_THREAD ); // 单块包含线程的数目
 
 #if USE_ELEMENT_SINGLE
 			nBlocksPerGrid.y = (PROBLEM_SIZE+nThreadsPerBlock.x - 1)/(nThreadsPerBlock.x * nBlocksPerGrid.x);
@@ -118,6 +129,15 @@ void initialize(int problem_size, int joint_size)
 	_vertexesStatic.initialize( problem_size, joint_size );
 #endif
 	_vertexesDynamic.initialize( problem_size, joint_size );
+
+	// cuda初始化，若不初始化将采用默认值
+	int i; // 有多个GPU时选择一个
+	float fGFLOPS = 0.0f;
+	i = gpuGetMaxGflopsDeviceId( fGFLOPS );
+    printf("计算能力估算公式=sp核数 * shader频率 \n\
+			计算能力粗略估算: %0.2f GFLOPS\n", fGFLOPS);
+    cudaSetDevice(i);
+
 }
 
 // 数据销毁：坐标、矩阵
@@ -128,4 +148,104 @@ void unInitialize()
 	_vertexesStatic.unInitialize();
 #endif
 	_vertexesDynamic.unInitialize();
+}
+
+#ifndef MIN
+#define MIN(a,b) ((a < b) ? a : b)
+#endif
+#ifndef MAX
+#define MAX(a,b) ((a > b) ? a : b)
+#endif
+// 查询每个SM包含GPU核心SP的个数
+int _ConvertSMVer2Cores(int major, int minor)
+{
+    // Defines for GPU Architecture types (using the SM version to determine the # of cores per SM
+    typedef struct {
+       int SM; // 0xMm (hexidecimal notation), M = SM Major version, and m = SM minor version
+       int Cores;
+    } sSMtoCores;
+
+	sSMtoCores nGpuArchCoresPerSM[] = 
+	{ { 0x10,  8 }, // Tesla Generation (SM 1.0) G80 class
+	  { 0x11,  8 }, // Tesla Generation (SM 1.1) G8x class
+	  { 0x12,  8 }, // Tesla Generation (SM 1.2) G9x class
+	  { 0x13,  8 }, // Tesla Generation (SM 1.3) GT200 class
+	  { 0x20, 32 }, // Fermi Generation (SM 2.0) GF100 class
+	  { 0x21, 48 }, // Fermi Generation (SM 2.1) GF10x class
+	  { 0x30, 192}, // Fermi Generation (SM 3.0) GK10x class
+	  {   -1, -1 }
+	};
+
+    int index = 0;
+    while (nGpuArchCoresPerSM[index].SM != -1) {
+       if (nGpuArchCoresPerSM[index].SM == ((major << 4) + minor) ) {
+          return nGpuArchCoresPerSM[index].Cores;
+       }	
+       index++;
+    }
+    printf("MapSMtoCores undefined SM %d.%d is undefined (please update to the latest SDK)!\n", major, minor);
+    return -1;
+}
+// 硬件拥有最大的浮点计算能力GFLOPS
+int gpuGetMaxGflopsDeviceId(float& fGFLOPS)
+{
+    int current_device     = 0, sm_per_multiproc  = 0;
+    int max_compute_perf   = 0, max_perf_device   = 0;
+    int device_count       = 0, best_SM_arch      = 0;
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceCount( &device_count );
+    
+    // Find the best major SM Architecture GPU device
+    while (current_device < device_count)
+    {
+        cudaGetDeviceProperties( &deviceProp, current_device );
+        if (deviceProp.major > 0 && deviceProp.major < 9999)
+        {
+            best_SM_arch = MAX(best_SM_arch, deviceProp.major);
+        }
+        current_device++;
+    }
+
+    // Find the best CUDA capable GPU device
+    current_device = 0;
+	while( current_device < device_count )
+	{
+		cudaGetDeviceProperties( &deviceProp, current_device );
+		if (deviceProp.major == 9999 && deviceProp.minor == 9999)
+		{
+			sm_per_multiproc = 1;
+		}
+		else
+		{
+			sm_per_multiproc = _ConvertSMVer2Cores(deviceProp.major, deviceProp.minor);
+		}
+
+		int compute_perf  = deviceProp.multiProcessorCount * sm_per_multiproc * deviceProp.clockRate;
+		// clockRate指shader的频率，单位是kHz，即"Clock frequency in kilohertz "，参考：http://developer.download.nvidia.com/compute/cuda/4_2/rel/toolkit/docs/online/structcudaDeviceProp_dee14230e417cb3059d697d6804da414.html#dee14230e417cb3059d697d6804da414
+
+		if( compute_perf  > max_compute_perf )
+		{
+			// If we find GPU with SM major > 2, search only these
+			if ( best_SM_arch > 2 )
+			{
+				// If our device==dest_SM_arch, choose this, or else pass
+				if (deviceProp.major == best_SM_arch)
+				{
+					max_compute_perf  = compute_perf;
+					max_perf_device   = current_device;
+				}
+			}
+			else
+			{
+				max_compute_perf  = compute_perf;
+				max_perf_device   = current_device;
+			}
+			printf("sp核数：%d=%d(SM个数)*%d(每个SM包含SP个数), shader频率: %d \n", deviceProp.multiProcessorCount * sm_per_multiproc, 
+				deviceProp.multiProcessorCount, sm_per_multiproc, deviceProp.clockRate);
+			SIZE_BLOCK = deviceProp.multiProcessorCount * 2; // 修改块数默认设置，动态设置为SM的两倍，确保每个SM有2个block
+		}
+		++current_device;
+	}
+	fGFLOPS = max_compute_perf * 1.0e-6;
+	return max_perf_device;
 }
