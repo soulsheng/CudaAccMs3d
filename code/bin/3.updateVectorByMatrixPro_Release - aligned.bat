73f65ef9a5524128b082b48c508b08a6c92295e4
gts250 <1806597655@qq.com>
2012-10-31 10:43:42 +0800

    批处理输出提示信息。 窗口最小化。

--- code/bin/3.updateVectorByMatrixPro_Release - aligned.bat
+++ code/bin/3.updateVectorByMatrixPro_Release - aligned.bat
@@ -4,6 +4,9 @@ cd Release
 
 if exist %exe%.txt del %exe%.txt
 
-for /l %%i in (0,1,6) do start /wait %exe% --problem=%%i --aligned=1
+for /l %%i in (0,1,6) do (
+start /wait /min %exe% --problem=%%i --quiet=1 --aligned=1
+)
+echo  aligned=1 >> %exe%.txt
 
 notepad %exe%
--- code/bin/4.updateVectorByMatrixPro_Release - separate.bat
+++ code/bin/4.updateVectorByMatrixPro_Release - separate.bat
@@ -5,6 +5,11 @@ cd Release
 if exist %exe%.txt del %exe%.txt
 
 for /l %%i in (0,1,2) do (
-for /l %%j in (0,1,6) do start /wait %exe% --problem=%%j --aligned=1 --separate=%%i )
+for /l %%j in (0,1,6) do (
+start /wait /min %exe% --problem=%%j --quiet=1 --aligned=1 --separate=%%i 
+)
+echo  aligned=1 >> %exe%.txt
+echo  separate=%%i >> %exe%.txt
+)
 
 notepad %exe%
