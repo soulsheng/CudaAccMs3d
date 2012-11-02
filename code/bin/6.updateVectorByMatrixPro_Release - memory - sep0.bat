1073b9206be7d5f58b87f1531cb4d7026ee0e919
gts250 <1806597655@qq.com>
2012-11-02 17:30:38 +0800

    统计共享存储的效率，gts250 跟常量存储相当，最终提升530% 。

--- code/bin/6.updateVectorByMatrixPro_Release - memory - sep0.bat
+++ code/bin/6.updateVectorByMatrixPro_Release - memory - sep0.bat
@@ -4,7 +4,7 @@ cd Release
 
 if exist %exe%.txt del %exe%.txt
 
-for /l %%i in (0,1,1) do (
+for /l %%i in (0,1,2) do (
 for /l %%j in (0,1,2) do (
 for /l %%k in (0,1,6) do (
 start /wait /min %exe% --problem=%%k --quiet=1 --aligned=1 --separate=0 --sort=%%j --memory=%%i
