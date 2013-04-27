46409bc192d5f2669fc29816638bbb33dd63f882
gts250 <1806597655@qq.com>
2013-04-27 11:23:52 +0800

    补充文件，准备转Ocl

---
+++ code/source/SimpleOptimizations/Joint.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <stdlib.h>
+#include <string.h>
+
+#define    MATRIX_SIZE_LINE    3//3
+
+//关节矩阵---------------------------------------------------------
+//typedef float4  Matrix[3];// 矩阵
+
+struct Joints{
+
+	// 获取关节矩阵 模拟
+	void initialize( int size ){
+		nSize = size;
+		//pMatrix = new float[nSize*MATRIX_SIZE_LINE*4];
+		pMatrix = (float*) _aligned_malloc(nSize*MATRIX_SIZE_LINE*4 * sizeof(float), 16);
+
+		for(int i=0;i<nSize;i++){
+			for(int j=0;j<MATRIX_SIZE_LINE;j++){
+				for(int k=0;k<4;k++){
+					pMatrix[i*MATRIX_SIZE_LINE*4 +4*j+k] = rand() * 1.0f;
+				}
+			}
+		}
+	}
+
+	// 释放空间
+	void unInitialize()
+	{
+		if (pMatrix)  _aligned_free(pMatrix);
+	}
+
+	float*  pMatrix;
+	int   nSize;// 关节的数目
+
+};// 关节的集合
+
---
+++ code/source/SimpleOptimizations/Vertex.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Joint.h"
+
+//顶点坐标---------------------------------------------------------
+//typedef float4 Vertex; // 坐标：(x,y,z);关节索引：w
+#define    VERTEX_VECTOR_SIZE    4
+
+struct Vertexes{
+
+	// 获取顶点坐标 模拟
+	void initialize(int size, int sizeJoint){
+		nSize = size;
+		//pVertex = new float[nSize*VERTEX_VECTOR_SIZE];
+		//pIndex = new int[nSize];
+		pVertex = (float*) _aligned_malloc(nSize*VERTEX_VECTOR_SIZE * sizeof(float), 16);
+		pIndex = (int*) _aligned_malloc(nSize * sizeof(int), 16);
+
+		for(int i=0;i<nSize;i++){
+			pVertex[i*VERTEX_VECTOR_SIZE + 0] = rand() * 1.0f;
+			pVertex[i*VERTEX_VECTOR_SIZE + 1] = rand() * 1.0f;
+			pVertex[i*VERTEX_VECTOR_SIZE + 2] = rand() * 1.0f;
+			pVertex[i*VERTEX_VECTOR_SIZE + 3] = 1.0f;
+			pIndex[i] = rand() % sizeJoint;
+		}
+	}
+
+	// 释放空间
+	void unInitialize()
+	{
+		if (pVertex) _aligned_free(pVertex);
+		if (pIndex) _aligned_free(pIndex);
+	}
+
+	float*  pVertex;
+	int   nSize;// 顶点的数目
+	int*		pIndex;
+};// 顶点的集合
